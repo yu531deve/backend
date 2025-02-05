19e3a5b0a4998cef8df7771479581e352edd3fbe
yu531deve <yuuyuu531yuu@gmail.com>
2025-02-06 05:35:44 +0900

    投稿データの保存状態を確認する前に進捗を保存

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   # 投稿一覧の取得
   def index
-    posts = Post.all
+    posts = Post.order(created_at: :desc) # ← ここで created_at の降順にする
     render json: posts
   end
+
 
   # 投稿の作成
   def create
@@ -50,6 +51,12 @@ class PostsController < ApplicationController
     render json: { error: "Post not found" }, status: :not_found
   rescue ActiveRecord::RecordInvalid => e
     render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  # すべてのお気に入り以外の投稿を削除
+  def destroy_all
+    Post.where(is_favorite: false).destroy_all # お気に入り以外の投稿のみ削除
+    render json: { message: "Non-favorite posts deleted" }, status: :ok
   end
 
 
